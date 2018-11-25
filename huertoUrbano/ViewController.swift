3679b7e52a05395714eac222e11b64572a5cdcfa
sara <sara_3000@msn.com>
2018-11-25 09:39:54 +0100

    Initial Commit

---
+++ huertoUrbano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  huertoUrbano
+//
+//  Created by Sara Lorente on 25/11/2018.
+//  Copyright © 2018 Sara Lorente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ huertoUrbanoTests/huertoUrbanoTests.swift
@@ -0,0 +1,34 @@
+//
+//  huertoUrbanoTests.swift
+//  huertoUrbanoTests
+//
+//  Created by Sara Lorente on 25/11/2018.
+//  Copyright © 2018 Sara Lorente. All rights reserved.
+//
+
+import XCTest
+@testable import huertoUrbano
+
+class huertoUrbanoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
